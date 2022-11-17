7b83a3fceeb85f274bc561a31e8dc3e3ccf9edcb
заебался жить <Hp@DESKTOP-BT44AN5>
2022-11-18 00:40:05 +0300

    Полная валидация вводимых данных при регистрации пользователя

--- Application/Validate/UserValidator.cs
+++ Application/Validate/UserValidator.cs
@@ -9,9 +9,39 @@ namespace Application.Validate
 {
     internal class UserValidator
     {
-        public string[] Validate(User)
+        public List<string> Validate(User user)
         {
-            
+            List<string> errors = new List<string>();
+            bool isNotCorrectLastName = string.IsNullOrWhiteSpace(user.LastName);
+            if (isNotCorrectLastName)
+            {
+                errors.Add("Неккоректная фамилия");
+            }
+            bool isNotCorrectFirstName = string.IsNullOrWhiteSpace(user.FirstName);
+            if (isNotCorrectFirstName)
+            {
+                errors.Add("Неккоректное имя");
+            }
+            return errors;
+            bool isNotCorrectAge = string.IsNullOrWhiteSpace(user.Age);
+            if (isNotCorrectAge)
+            {
+                errors.Add("Неккоректный возраст");
+            }
+            return errors;
+            bool isNotCorrectLogin = string.IsNullOrWhiteSpace(user.Login);
+            if (isNotCorrectLogin)
+            {
+                errors.Add("Неккоректный логин");
+            }
+            return errors;
+
+            bool isNotCorrectPassword = string.IsNullOrWhiteSpace(user.Password);
+            if (isNotCorrectPassword)
+            {
+                errors.Add("Неккоректный пароль");
+            }
+            return errors;
         }
 
 
