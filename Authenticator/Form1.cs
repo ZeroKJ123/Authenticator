591a3cb00dc3872a64cc9f49cc03861a16ea71bd
Zero <98895459+ZeroKJ123@users.noreply.github.com>
2022-11-17 23:02:04 +0300

    Добавлена форма регистраци

--- Authenticator/Form1.cs
+++ Authenticator/Form1.cs
@@ -20,7 +20,7 @@ namespace Authenticator
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) 
         {
 
         }
