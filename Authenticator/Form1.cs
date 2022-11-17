67e60b467de2d3037840e94da603fd72588f9fd0
Zero <98895459+ZeroKJ123@users.noreply.github.com>
2022-11-17 22:47:33 +0300

    Добавленно поле number

--- Authenticator/Form1.cs
+++ Authenticator/Form1.cs
@@ -13,7 +13,9 @@ namespace Authenticator
     public partial class Form1 : Form
     {
         
-        public Form1()
+        private int number;
+
+        public Form1() 
         {
             InitializeComponent();
         }
