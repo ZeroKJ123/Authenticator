e68f1f99c776f7a94ecd6aa5eb60717a97db028d
заебался жить <Hp@DESKTOP-BT44AN5>
2022-11-18 00:14:19 +0300

    Я под командованием мафленда добавил валидацию вводимых данных при регистрации(имени фамилии и т.д)

---
+++ Application/Validate/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validate
+{
+    internal class UserValidator
+    {
+        public string[] Validate(User)
+        {
+            
+        }
+
+
+
+
+    }
+}
