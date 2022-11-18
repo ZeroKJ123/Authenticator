ba28d354e1a4dfd623237e5f88f6507bde4a47cb
maks233online <maks233online@gmail.com>
2022-11-18 22:23:15 +0300

    Auth-1 Добавил регистрацию.

---
+++ Application/Dtos/RegistrationResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Dtos;
+
+/// <summary>
+/// Dto ответа регистрации.
+/// </summary>
+public class RegistrationResponseDto
+{
+    /// <summary>
+    /// Регистрация успешна.
+    /// </summary>
+    public required bool Success { get; init; }
+
+    /// <summary>
+    /// Ошибки.
+    /// </summary>
+    public required List<string> Errors { get; init; }
+}
